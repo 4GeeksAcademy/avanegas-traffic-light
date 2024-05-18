009c0044fac165d459a0b8a7b270a3fd8db42d2f
Angie Vanegas P <arco527@gmail.com>
2024-05-18 23:20:13 +0000

    Add colors styles dine

---
+++ src/js/component/Colors.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Colors = () =>{
+    return (
+
+        <div className="trafficlight">
+            <div className="green"></div>
+            <div className="yellow"></div>
+            <div className="red"></div>
+        </div>
+        
+    )
+
+
+}
+
+export default Colors;
